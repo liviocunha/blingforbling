1daa6fd3e75c4cac8d323c744f877aab64089176
Lívio Cunha <liviocunha91@hotmail.com>
2021-03-13 18:01:37 -0300

    Model Bling created

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Bling(models.Model):
+    """
+    Different accounts of Bling ERP.
+    Fields:
+        name (str): Name given to Bling Account
+        api_key (str): API key provided by Bling
+     """
+    name = models.CharField(max_length=20)
+    api_key = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
