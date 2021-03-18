9176831c6ee74096689d07cb1c4112b44d534061
Lívio Cunha <liviocunha91@hotmail.com>
2021-03-17 23:12:26 -0300

    Account login route

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from .views import HookInventoryChangeView, home, insert_products
 
 urlpatterns = [
     path('', home),
     path('hookinventory/', HookInventoryChangeView.as_view(), name='Hook Inventory Change'),
     path('insertProducts/', insert_products),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
-
