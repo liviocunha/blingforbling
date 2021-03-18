007df05e4fce5d9ca8828c070611c6e9015e28a0
Lívio Cunha <liviocunha91@hotmail.com>
2021-03-18 15:05:23 -0300

    Fixing bug that the both callbacks were call in the same view

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-from .views import HookInventoryChangeView1,HookInventoryChangeView2, home, insert_products
+from .views import HookInventoryChangeView1, HookInventoryChangeView2, home, insert_products
 
 urlpatterns = [
     path('', home),
     path('hookinventory/1/', HookInventoryChangeView1.as_view(), name='Hook Inventory Change Bling 1'),
-    path('hookinventory/2/', HookInventoryChangeView1.as_view(), name='Hook Inventory Change Bling 2'),
+    path('hookinventory/2/', HookInventoryChangeView2.as_view(), name='Hook Inventory Change Bling 2'),
     path('insertProducts/', insert_products),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
