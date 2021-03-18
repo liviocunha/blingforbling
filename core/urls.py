bb1dda8e4575ae3ace2e28ae487d7e6ab16d8aa4
Lívio Cunha <liviocunha91@hotmail.com>
2021-03-18 11:04:20 -0300

    Defining routes for receiving callback from both accounts

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import HookInventoryChangeView, home, insert_products
+from .views import HookInventoryChangeView1,HookInventoryChangeView2, home, insert_products
 
 urlpatterns = [
     path('', home),
-    path('hookinventory/', HookInventoryChangeView.as_view(), name='Hook Inventory Change'),
+    path('hookinventory/1/', HookInventoryChangeView1.as_view(), name='Hook Inventory Change Bling 1'),
+    path('hookinventory/2/', HookInventoryChangeView1.as_view(), name='Hook Inventory Change Bling 2'),
     path('insertProducts/', insert_products),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
